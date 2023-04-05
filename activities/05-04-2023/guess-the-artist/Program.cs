2a51cd08f862834a588c3ddbb3aedcf5e9643795
juliaathar <juathar@gmail.com>
2023-04-05 10:40:45 -0300

    guess the artist programm

---
+++ activities/05-04-2023/guess-the-artist/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine($"Bem-vindo ao nosso quiz, adivinhe a idade da Cantora Rihanna :)");
+
+int answer = 0;
+
+do 
+{
+    Console.WriteLine($"Qual é a idade Rihanna?");
+    answer = int.Parse (Console.ReadLine());
+} 
+
+while ( answer != 35);
+
+Console.WriteLine($"Você acertou! Shine bright like a diamond!");
