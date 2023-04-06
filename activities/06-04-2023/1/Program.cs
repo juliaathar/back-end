0ee2595425e55ce0c2b2f7e6af3064fda00ec24c
juliaathar <juathar@gmail.com>
2023-04-06 08:03:14 -0300

    finished activity 1

---
+++ activities/06-04-2023/1/Program.cs
@@ -0,0 +1,12 @@
+﻿// 1 - Faça um programa que peça uma nota, entre zero e dez. Mostre uma
+// mensagem caso o valor seja inválido e continue pedindo até que o usuário
+// informe um valor válido.
+
+Console.WriteLine($"Digite uma nota de 0 a 10!");
+int note = int.Parse (Console.ReadLine());
+
+while (note < 0 || note > 10)
+{
+    Console.WriteLine($"Valor inválido. Digite uma nota de 0 a 10:");
+    note = int.Parse (Console.ReadLine());
+}
