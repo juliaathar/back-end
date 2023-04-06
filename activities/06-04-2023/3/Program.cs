b42be8338dc57bbf8e4c1e8c51edc8b687811815
juliaathar <juathar@gmail.com>
2023-04-06 08:22:36 -0300

    finished activity 3

---
+++ activities/06-04-2023/3/Program.cs
@@ -0,0 +1,10 @@
+﻿// Faça um programa que receba um número inteiro e mostre a sua tabuada.
+
+Console.WriteLine($"Digite um número inteiro e receba sua tabuada!");
+int n = int.Parse(Console.ReadLine());
+
+for (int i = 1; i <= 10; i++)
+{
+    int result = n * i;
+    Console.WriteLine($"{n} * {i} = {result}");
+}
