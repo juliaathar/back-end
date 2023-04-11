ab06c82970c27583b3ec2e22f15671dd9ca4aab6
juliaathar <juathar@gmail.com>
2023-04-11 10:28:09 -0300

    activity for and foreach

---
+++ activities/11-04-2023/1/Program.cs
@@ -0,0 +1,12 @@
+﻿int[] numeros = new int[5];
+
+for (int i = 0; i < 5; i++)
+{
+    Console.Write($"Digite o {i + 1}º número e veja seu valor dobrado!");
+    numeros[i] = int.Parse(Console.ReadLine());
+}
+
+foreach (var numero in numeros)
+{
+    Console.WriteLine($"O dobro do número {numero} é {numero*2}!");
+}
