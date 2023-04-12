0875084af697b51bddf172334873c5ed07b0aac2
juliaathar <juathar@gmail.com>
2023-04-12 10:51:38 -0300

    test methods

---
+++ activities/12-04-2023/2/Program.cs
@@ -0,0 +1,12 @@
+﻿static void barraCarregamento(string texto, int quantidadePontinhos, int tempo)
+{
+    Console.Write(texto);
+
+    for (var i = 0; i < quantidadePontinhos; i++)
+    {
+        Console.Write($".");
+        Thread.Sleep(tempo);
+    }
+}
+
+barraCarregamento("Carregando", 5, 1000);
