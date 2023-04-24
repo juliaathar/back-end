6252616d65e30e9e5635cfe28b9f4b778861d428
juliaathar <juathar@gmail.com>
2023-04-24 09:18:50 -0300

    ...

--- activities/24-04-2023/calculator class activity/Calculadora.cs
+++ activities/24-04-2023/calculator class activity/Calculadora.cs
@@ -3,28 +3,26 @@ namespace calculator_class_activity
 {
     public class Calculadora
     {
-        public float n1;
-        public float n2;
         public void Soma(float n1, float n2)
         {
             float resultado = n1 + n2;
-            Console.WriteLine($"O resultado e {resultado}");
+            Console.WriteLine($"O resultado da soma é {resultado}");
             
         }
         public void Subtracao(float n1, float n2)
         {
             float resultado = n1 - n2;
-            Console.WriteLine($"O resultado e {resultado}");
+            Console.WriteLine($"O resultado da subtração é {resultado}");
         }
         public void Multiplicacao(float n1, float n2)
         {
             float resultado = n1 * n2;
-            Console.WriteLine($"O resultado e {resultado}");
+            Console.WriteLine($"O resultado multiplicação é {resultado}");
         }
         public void Divisao(float n1, float n2)
         {
             float resultado = n1 / n2;
-            Console.WriteLine($"O resultado e {resultado}");
+            Console.WriteLine($"O resultado divisão é {resultado}");
         }
     }
 }
--- activities/24-04-2023/calculator class activity/Program.cs
+++ activities/24-04-2023/calculator class activity/Program.cs
@@ -1,23 +1,23 @@
 ﻿using calculator_class_activity;
 
-Console.WriteLine(@$"Bem vindo a nossa calculadora escolha sua operacao:
+Console.WriteLine(@$"Bem vindo a nossa calculadora escolha sua operação:
 1- soma
-2- subtracao 
-3- multiplicacao
-4- divisao");
+2- subtração 
+3- multiplicação
+4- divisão");
 char operacao = char.Parse(Console.ReadLine());
 
-Console.WriteLine($"Agora informe seu primeiro numero:");
+Console.WriteLine($"Agora informe seu primeiro número:");
 float n1 = float.Parse(Console.ReadLine());
 
-Console.WriteLine($"Agora informe seu segundo numero:");
+Console.WriteLine($"Agora informe seu segundo número:");
 float n2 = float.Parse(Console.ReadLine());
 
 Calculadora calculando = new Calculadora();
 
 if (operacao == '1')
 {
-    calculando.Soma(n1, n2);
+    calculando.Soma(n1, n2); 
 }
 else if (operacao == '2')
 {
