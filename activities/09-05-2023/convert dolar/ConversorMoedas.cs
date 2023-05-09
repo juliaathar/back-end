d2d15d17343df3727f3a2cfcc21e33e97a448981
juliaathar <juathar@gmail.com>
2023-05-09 10:06:27 -0300

    finished

--- activities/09-05-2023/convert dolar/ConversorMoedas.cs
+++ activities/09-05-2023/convert dolar/ConversorMoedas.cs
@@ -1,12 +1,30 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
 namespace convert_dolar
 {
     public class ConversorMoedas
     {
-        
+        public static float ValorDolar { get; private set; } = 5.01f;
+
+        public static float DolarToreal()
+        {
+            Console.WriteLine($"Qual a quantidade de dolares?");
+            float dolar = float.Parse(Console.ReadLine());
+
+            float real = ValorDolar * dolar;
+            Console.WriteLine($"Valor em reais {real}");
+
+            return real;
+
+        }
+        public static float RealToDolar()
+        {
+            Console.WriteLine($"Qual a quantidade de reais?");
+            float real = float.Parse(Console.ReadLine());
+
+            float dolar = real / ValorDolar;
+            Console.WriteLine($"Valor em reais {dolar}");
+            return dolar;
+
+        }
+
     }
 }
--- activities/09-05-2023/convert dolar/Program.cs
+++ activities/09-05-2023/convert dolar/Program.cs
@@ -1,3 +1,41 @@
 ﻿// Exercício : Faça um programa de conversão de moedas(câmbio) usando classe, membro e métodos estáticos.
 // Menu de opções : dólar para real, real para dólar, sair
 // Personalizar a resposta, usando recursos da linguagem(Bibliotecas)
+
+using convert_dolar;
+
+// Console.WriteLine(ConversorMoedas.DolarToreal(30));
+// Console.WriteLine(ConversorMoedas.RealToDolar(100));
+
+char opcao;
+
+do
+{
+    Console.WriteLine(@$"
+    _______________________________________________
+    |                                             |
+    |   Bem-vindo ao nosso conversor de moedas!   |
+    |           Escolha sua operação:             |
+    |                                             |
+    |           1 - Dólar para real               |
+    |           2 - Real para dólar               |
+    |           0 - Sair                          |
+    |                                             |
+    |_____________________________________________|                             
+    ");
+    opcao = char.Parse(Console.ReadLine());
+
+    switch (opcao)
+    {
+        case '1':
+            ConversorMoedas.DolarToreal();
+            break;
+        case '2':
+        ConversorMoedas.RealToDolar();
+            break;
+        default:
+            break;
+    }
+
+
+} while (opcao != 0);
