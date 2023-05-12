13b28d80f2e43a555a47cd6b1da49f6f85d0c3fd
juliaathar <juathar@gmail.com>
2023-05-12 07:57:46 -0300

    finished

--- classes/object-lists/Program.cs
+++ classes/object-lists/Program.cs
@@ -2,6 +2,7 @@
 
 using object_lists;
 
+// CREATE
 List<Produto> produtos = new List<Produto>();
 
 // inserir objetos de produtos dentro da lista, há duas formas de fazer isso:
@@ -18,4 +19,41 @@ produtos.Add(camisetaArmani);
 foreach (var item in produtos)
 {
     Console.WriteLine($"Código: {item.Codigo}, Nome: {item.Nome}, Preço: {item.Preco:C}");
+}
+
+// UPDATE
+
+// acessar um objeto da lista para poder manipular
+Produto produtoEncontrado = produtos.Find(x => x.Codigo == 1235);
+
+// encontrar o indice do produto encontrado
+int index = produtos.IndexOf(produtoEncontrado);
+
+// exibir o indice no console
+Console.WriteLine($"O indice do produto encontrado e o: {index}");
+
+// fazer alteracoes 
+produtoEncontrado.Preco = 599.99f;
+
+// remover item antigo da lista
+produtos.RemoveAt(index);
+
+// devolver o item atualizado no lugar
+produtos.Insert(index,produtoEncontrado);
+
+Console.WriteLine($"Produtos depois da atualizacao");
+
+
+foreach (var item in produtos)
+{
+    Console.WriteLine($"Código: {item.Codigo}, Nome: {item.Nome}, Preço: {item.Preco:C}");  
+}
+
+// DELETE 
+
+produtos.RemoveAt(1);
+
+foreach (var item in produtos)
+{
+    Console.WriteLine($"Código: {item.Codigo}, Nome: {item.Nome}, Preço: {item.Preco:C}");  
 }
